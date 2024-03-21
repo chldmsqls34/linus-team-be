dfaef440b8c23cd2a0615673730d33ba65958be0
kimpuro <puro@protonmail.ch>
2024-03-21 17:21:09 +0900

    Add JPA annotations in Account and User classes
    Jakarta Persistence API (JPA) annotations have been added to the Account and User classes to manage their persistence. This change includes importing JPA annotations and defining important fields, including "id", "username", and "password". Also, it establishes one-to-many relationships between User and the Account and Order classes.

--- src/main/java/com/linus/api/account/Account.java
+++ src/main/java/com/linus/api/account/Account.java
@@ -1,5 +1,10 @@
 package com.linus.api.account;
 
+import com.linus.api.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.util.Date;
@@ -9,14 +14,19 @@ import java.util.Date;
 @ToString(exclude = {"id"})
 
 public class Account {
+    @Id
+    @Column(name="id")
     private Long id; //아이디
-
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user; //유저
+    @Column(name="account_number")
     private String accountNumber; //계좌번호
-
+    @Column(name="account_holder")
     private String accountHolder; // 예금주
-
+    @Column(name="balance")
     private Double balance; //잔고
-
+    @Column(name="transaction_date")
     private Date transactionDate;  //현재날짜
 
     @Builder(builderMethodName = "builder")
--- src/main/java/com/linus/api/user/User.java
+++ src/main/java/com/linus/api/user/User.java
@@ -1,7 +1,11 @@
 package com.linus.api.user;
 
+import com.linus.api.account.Account;
+import com.linus.api.order.Order;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity(name="users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -9,16 +13,28 @@ import lombok.*;
 @ToString(exclude = {"id"})
 public class User {
     @Id
-    @Column(name="id",nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+    @OneToMany(mappedBy = "user")
+    private List<Account> accounts;
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="username")
     private String username;
+    @Column(name="password")
     private String password;
+    @Column(name="name")
     private String name;
+    @Column(name="phone")
     private String phone;
+    @Column(name="address")
     private String address;
+    @Column(name="job")
     private String job;
+    @Column(name="height")
     private double height;
+    @Column(name="weight")
     private double weight;
 
     @Builder(builderMethodName = "builder")

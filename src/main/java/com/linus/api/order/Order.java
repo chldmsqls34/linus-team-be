41fa0eaa7fab96f73592819a1362ec8c5c18a557
HongGyun <dlaghdrbs907@gmail.com>
2024-03-21 17:51:14 +0900

    240321 : ManytoMany order - product - user

--- src/main/java/com/linus/api/order/Order.java
+++ src/main/java/com/linus/api/order/Order.java
@@ -1,4 +1,24 @@
 package com.linus.api.order;
 
+import com.linus.api.product.Product;
+import com.linus.api.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+
 public class Order {
+  @Id
+  @Column(name="id",nullable = false)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+
 }

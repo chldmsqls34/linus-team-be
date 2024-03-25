8c0ccdf43af852c23e81460f7e94fba8b3a32be7
chldmsqls34 <chldmsqls34@gmail.com>
2024-03-25 18:36:10 +0900

    Feat : Add login function

--- src/main/java/com/linus/api/user/UserRepository.java
+++ src/main/java/com/linus/api/user/UserRepository.java
@@ -3,7 +3,10 @@ package com.linus.api.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsername(String username); // String 부분에 Object들어오면안되고 username의 타입이 와야한다.
 }
